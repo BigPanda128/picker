7cea0ee0c8b740f1710e1045cda3b9dd81b307a3
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-10 23:38:05 -0500

    10 sided dice is working

--- chooseOne.js
+++ chooseOne.js
@@ -1,6 +1,6 @@
 var courses = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
 
-var courses = { "4": ['1','2','3','4'], "6": ['1','2','3','4','5','6'], "cmp344": ['Craig Austgen','Daniel Davis','Dylan Gainey','Lucas Grube','Eric Heinzman','Katelyn Herre','Phillip Hofer','Andrew Klump','Ryan Postma','Nathan Samano','Danilo Zekovic'], "cmp341": ['Craig Austgen','Lucas Grube','Eric Heinzman','Phillip Hofer','Nathan Samano','Danilo Zekovic']}
+var courses = { "4": ['1','2','3','4'], "6": ['1','2','3','4','5','6'], "10": ['1','2','3','4','5','6','7','8','9','10'], "cmp341": ['Craig Austgen','Lucas Grube','Eric Heinzman','Phillip Hofer','Nathan Samano','Danilo Zekovic']}
  
 
 function chooseOne(course) {
